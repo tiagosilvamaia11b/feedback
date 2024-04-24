14b1a8a10826345f9af583986c8055427ca5eefb
tiago maia <tmaia5003@gmail.com>
2024-04-24 14:26:25 -0300

    javascript do agradecimento1

--- pages/agradecimento.js
+++ pages/agradecimento.js
@@ -1,7 +1,9 @@
 function pegarNota() {
     let nota = localStorage.getItem("nota")
 
-    console.log(nota)
+    let paragrafoNota = document.querySelector(".paragrafo_nota")
+
+    paragrafoNota.innerHTML = `Você selecionou ${nota} de 5`
 }
 
 pegarNota()
